2b7152104e22eb506ce341357d5f563c74fd49c2
agnespravida <agnespravida27@gmail.com>
2020-11-20 06:14:21 +0700

    final

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -1,21 +1,32 @@
-const {restaurant, menu} = require('../models')
+const { Restaurant, Menu, User } = require('../models')
 
 class ControllerRestaurant { 
     static showRestaurantMenu(req, res){
-        restaurant
-            .findAll({
-                order : [['id', 'ASC']]
+        Restaurant
+            .findByPk(+req.params.id, {
+                include: Menu
             })
             .then(data => {
-                res.render('menu', {data})
+                let menu = []
+                for (let i = 0; i < data.Menus.length; i++){
+                    let template = {
+                        id: data.Menus[i].id,
+                        name: data.Menus[i].name,
+                        price: data.Menus[i].formatPrice(),
+                        description: data.Menus[i].description
+                    }
+                    menu.push(template)
+                }
+                res.render("menu.ejs", {menu, data})
             })
-            .catch(err =>{
-                res.send(err)
-            })  
+            .catch(err => {
+                res.send(err.message)
+            })
     }
 
     static addMenu(req, res){
-        res.render('addMenu')
+        let id = req.params.id
+        res.render('addMenu', { id })
     }
 
     static add(req, res){
@@ -23,12 +34,13 @@ class ControllerRestaurant {
             name : req.body.name,
             price : req.body.price,
             amount : req.body.amount,
-            description : req.body.description
+            description : req.body.description,
+            RestaurantId: req.params.id
         }
-        menu
+        Menu
             .create(newMenu)
             .then(data =>{
-                res.redirect('menu')
+                res.redirect(`/restaurants/menus/${req.params.id}`)
             })
             .catch(err =>{
                 res.send(err)
@@ -38,7 +50,7 @@ class ControllerRestaurant {
     static editMenu(req, res){
         const id = +req.params.id
 
-        menu
+        Menu
             .findByPk(id)
             .then(data =>{
                 res.render('editMenu', {data})
@@ -57,14 +69,18 @@ class ControllerRestaurant {
             description : req.body.description
         }
 
-        menu
+        Menu
         .update(updatedMenu, {
             where : {
                 id : id
             }
         })
         .then(data =>{
-            res.redirect('menu')
+            return Menu.findByPk(id)
+            //res.redirect(`/restaurants/profile/`)
+        })
+        .then(data => {
+            res.redirect(`/restaurants/profile/${data.RestaurantId}`)
         })
         .catch(err => {
             res.send(err)
@@ -73,18 +89,46 @@ class ControllerRestaurant {
 
     static delete(req, res){
         const id = +req.params.id
-        menu 
-            .destroy({
-                where : {
-                    id : id
-                }
+        let restoId = null
+        Menu.findByPk(id)
+            .then(data => {
+                restoId = data.RestaurantId
+                return  Menu.destroy({
+                    where : {
+                        id : id
+                    }
+                })
             })
             .then(data =>{
-                res.redirect('menu')
+                res.redirect(`/restaurants/menus/${restoId}`)
             })
             .catch(err =>{
-                res.send(err)
+                res.send(err.message)
             })
+    }
+
+    static showRestaurantProfil(req, res){
+        Restaurant.findByPk(+req.params.id)
+        .then(data => {
+            res.render("./restaurants/restaurantProfile.ejs", { data })
+        })
+        .catch(err => {
+            res.send(err)
+        })
+    }
+
+    static showReservations(req, res){
+        Restaurant.findByPk(+req.params.id, {
+            include: User
+          })
+          .then(data => {
+            res.send(data)
+            //res.render("./restaurants/reservations.ejs", { reservation: data.Users })
+          })
+          .catch(err => {
+            res.send(err.message)
+          })
+
     }
 };
 
